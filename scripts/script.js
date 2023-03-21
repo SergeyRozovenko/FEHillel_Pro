c0db2ae5c0a438d77fbd1c96cf493d41a430b691
SergeyRozovenko <s.rozovenko@gmail.com>
2023-03-21 12:14:14 +0200

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,31 @@
-energy = 12;
-weight = 9;
-space = '<br />' + '<br />';
-result = energy / weight;
+let age1, age2, age3;
+let firstName1, firstName2, firstName3;
+let lastName1, lastName2, lastName3;
 
-document.write('***************' ,space,
-'Ускорение тела при силе F = ', energy, ' и массе m = ' ,weight, ':' ,space,
-'--------------------', space,
-'a = ',result, space,
-'--------------------' ,space,
-`end.`);
+// Отримання віку та імен користувачів
+age1 = prompt("Введіть вік першого студента:");
+firstName1 = prompt("Введіть ім'я першого студента:");
+lastName1 = prompt("Введіть прізвище першого студента:");
+
+age2 = prompt("Введіть вік другого студента:");
+firstName2 = prompt("Введіть ім'я другого студента:");
+lastName2 = prompt("Введіть прізвище другого студента:");
+
+age3 = prompt("Введіть вік третього студента:");
+firstName3 = prompt("Введіть ім'я третього студента:");
+lastName3 = prompt("Введіть прізвище третього студента:");
+
+space = '\n' + '\n';
+
+// Обчислення середнього віку
+const averageAge = (parseInt(age1) + parseInt(age2) + parseInt(age3)) / 3;
+
+console.log('**************' ,space,
+'Список студентів, що зареєструвалися на вебінар :',space, 
+'1.',age1,'років', firstName1, lastName1,space,
+'2. ' ,age2,'років', firstName2, lastName2, space,
+'3. ' ,age3,'років', firstName3, lastName3, space,
+'--------------' ,space,
+'Середній вік студента :',averageAge, space,
+'--------------' ,space,
+'**************' );
