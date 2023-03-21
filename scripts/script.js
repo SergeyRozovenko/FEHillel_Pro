5adbe74feba4890c7395af4db15171303fe3db13
SergeyRozovenko <s.rozovenko@gmail.com>
2023-03-21 10:54:18 +0200

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 energy = 12;
 weight = 9;
+space = '<br />' + '<br />';
 result = energy / weight;
 
-document.write('***************' + '<br>' + '<br>' + 
-'Ускорение тела при силе F = ', energy, ' и массе m = ' ,weight, ':' + '<br>' + '<br>' + 
-'--------------------' + '<br>' + '<br>' +  
-'a = ',result, '<br>' + '<br>' + 
-'--------------------' + '<br>' + '<br>' + 
+document.write('***************' ,space,
+'Ускорение тела при силе F = ', energy, ' и массе m = ' ,weight, ':' ,space,
+'--------------------', space,
+'a = ',result, space,
+'--------------------' ,space,
 `end.`);
