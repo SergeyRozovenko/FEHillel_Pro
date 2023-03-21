b8c1e8d5a77b17e6c4f5eafe358f7eed9d230335
SergeyRozovenko <s.rozovenko@gmail.com>
2023-03-21 19:46:02 +0200

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,25 @@
-let age1, age2, age3;
-let firstName1, firstName2, firstName3;
-let lastName1, lastName2, lastName3;
 
-// Отримання віку та імен користувачів
-age1 = prompt("Введіть вік першого студента:");
-firstName1 = prompt("Введіть ім'я першого студента:");
-lastName1 = prompt("Введіть прізвище першого студента:");
+const student1 =  prompt("Введите возраст первого студента") + ' років' +
+                  prompt("Введите имя первого студента")  + ' ' +
+                  prompt("Введите фамилию первого студента");
+                   
+const student2 =  prompt("Введите возраст второго студента") + ' років' + 
+                  prompt("Введите имя второго студента")  + ' ' +
+                  prompt("Введите фамилию второго студента");
 
-age2 = prompt("Введіть вік другого студента:");
-firstName2 = prompt("Введіть ім'я другого студента:");
-lastName2 = prompt("Введіть прізвище другого студента:");
+const student3 =  prompt("Введите возраст третьего студента") + ' років'  +
+                  prompt("Введите имя третьего студента") + ' ' +
+                  prompt("Введите фамилию третьего студента");
 
-age3 = prompt("Введіть вік третього студента:");
-firstName3 = prompt("Введіть ім'я третього студента:");
-lastName3 = prompt("Введіть прізвище третього студента:");
+const space = '\n' + '\n';
 
-space = '\n' + '\n';
-
-// Обчислення середнього віку
-const averageAge = (parseInt(age1) + parseInt(age2) + parseInt(age3)) / 3;
+const averageAge = ((parseInt(student1) + parseInt(student2) + parseInt(student3)) / 3);
 
 console.log('**************' ,space,
 'Список студентів, що зареєструвалися на вебінар :',space, 
-'1.' ,age1,'років', firstName1, lastName1, space,
-'2.' ,age2,'років', firstName2, lastName2, space,
-'3.' ,age3,'років', firstName3, lastName3, space,
+'1.' ,student1, space,
+'2.' ,student2, space,
+'3.' ,student3, space,
 '--------------' ,space,
 'Середній вік студента :',averageAge, space,
 '--------------' ,space,
