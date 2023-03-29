fb69bda9a8143400b208ea189481942f836ad47c
SergeyRozovenko <s.rozovenko@gmail.com>
2023-03-29 11:35:53 +0300

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-let arr = New Array();
+let arr_A = [];
+
+arr_A.length = 15;
+
+for (i = 0; i < arr_A.length; i++) {
+  arr_A[i] = Math.floor(Math.random() * 10)
+
+  if (i % 2 !== 0) {
+    arr_A[i] = 0
+  }
+}
+
+console.log(arr_A);
