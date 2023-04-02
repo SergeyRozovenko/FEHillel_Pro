d152dee9ae8be9d10049b6ebbc00f480e6ff2057
SergeyRozovenko <s.rozovenko@gmail.com>
2023-04-02 20:12:43 +0300

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
-const arr = [1,2,3,4,5,6,7];
+const arr = [1,2,3,4,5,6,7,8];
+console.log (arr);
 
 for (let i = 0; i < arr.length - 1; i += 2) {
-  console.log (i);
+  console.log (arr[i]);
   const temp = arr[i];
   arr[i] = arr[i + 1];
   arr[i + 1] = temp;
