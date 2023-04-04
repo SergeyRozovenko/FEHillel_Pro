3efaefac0f251b930d56097538204cd8ff065ad1
SergeyRozovenko <s.rozovenko@gmail.com>
2023-04-04 23:17:48 +0300

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-function removeElement(array, element) {
-
-  const index = array.indexOf(element);
-
-  if (index !== -1) {
-
-    array.splice(index, 1);
-
+function removeElements(array, items) {
+  for (let i = 0; i < array.length; i++) {
+    if (items.includes(array[i])) {
+      array.splice(i, 1);
+      i--;
+    }
   }
-
+  return array;
 }
 
-
-
+const array = [1, 2, 3, 3, 4, 5, 6, 7];
+removeElements(array, [3]);
+console.log(array);
+// Результат: [1, 2, 3, 4, 6, 7]
