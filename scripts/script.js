eb8b131cbd6aed0983bad4d9ab63c79aa9159474
SergeyRozovenko <s.rozovenko@gmail.com>
2023-04-05 23:18:00 +0300

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 function generateKey(length, characters) {
-  let result = '';
-  const charactersLength = characters.length;
-for (let i = 0; i < length; i++) {
-const randomIndex = Math.floor(Math.random() * charactersLength);
-result += characters.slice(randomIndex, randomIndex + 1);
-}
-  return result;
+	let result = '';
+	const charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * charactersLength);
+		result += characters.slice(randomIndex, randomIndex + 1);
+	}
+	return result;
 }
 
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
