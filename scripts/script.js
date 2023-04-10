cd0fb2d245f0cdb8dd16e413f7f4cf7670047d1f
SergeyRozovenko <s.rozovenko@gmail.com>
2023-04-10 15:24:48 +0300

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -10,4 +10,24 @@ function filter(array, callback) {
   
   const input = [1, 2, 3, 4, 5, 6];
   function isEven(x) { return x % 2 == 0; }
-  console.log(filter(input, isEven)); // [2, 4, 6]
+  console.log(filter(input, isEven)); 
+
+//===============TASK 2
+
+  function copy(list, callback) {
+	const newList = [];
+	for (let i = 0; i < list.length; i++) {
+		let element;
+		if (callback) {
+		  element = callback(list[i]);
+		} else {
+		  element = list[i];
+		}
+	  newList.push(element);
+	}
+	return newList;
+  }
+  
+  const list = [1, 2, 3, 4, 5];
+  const newL = copy(list, (value) => value * 10);
+  console.log(newL); 
