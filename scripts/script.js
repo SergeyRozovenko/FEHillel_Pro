eb0793a31d2fcb07f840c096878c372391c0d760
SergeyRozovenko <s.rozovenko@gmail.com>
2023-05-03 22:29:56 +0300

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,64 @@
-function unification(mass) {
-    let result = [];
-    for (let i = 0; i < mass.length; i++) {
-      if (Array.isArray(mass[i])) {
-        result = result.concat(unification(mass[i])); 
-      } else {
-        result.push(mass[i]); 
-      }
-    }
-    return result;
+
+async function getAllProducts() {
+  const response = await fetchAllProducts();
+  const prouducts = response.products;
+
+  console.log(prouducts, "prouducts");
+
+
+  const productsTamplate = `
+ <article class="products">
+      ${prouducts.map((product) => `
+          <section class="product-item">
+               <div class="image-wrapper">
+                  ${getProductImage(product.thumbnail)}
+              </div>
+              <div class="content-wrapper">
+                  <div class="title">
+                      ${getProductTitle(product.title)}
+                  </div>
+                  <div class="price">
+                      ${getProductPrice(product.price, product.discountPercentage)}
+                  </div>
+                  <div class="description">${getProductDescription(product.description)}</div>
+                  <div class="actions">
+                      <button id="cart"class="button green-solid cart">
+                          Add to Cart
+                      </button>
+                      <button class="button more">More Details</button>
+                  </div>
+              </div>
+          </section>  
+      `).join("")}
+ </article>
+`;
+  document.getElementById("app").innerHTML = productsTamplate;
+}
+
+
+const getProductTitle = (title) => `<h4>${title}</h4>`
+
+const getProductPrice = (price, discount) => {
+  const discountPrice = ((price * 100 - (discount / 100) * (price * 100))) / 100;
+
+  return `
+  <span><${price}, price with discount ${discountPrice}</span>
+  `
+}
+
+const getProductDescription = (description) =>  `<span>${description}</span>`
+
+const getProductImage = (url) => {
+  if (!url) {
+      return <span>Not Found</span>
   }
-  
-  let mass = [[1,2,3, [3.1]], 4, [5,6, [7, 8]]];
-  console.log(unification(mass));
+  return `
+      <img
+          src="${url}"
+          alt=""
+          class="image"
+      />
+  `
+}
+
+getAllProducts();
