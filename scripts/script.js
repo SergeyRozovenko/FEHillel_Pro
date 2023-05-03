e1a7da465d3c1cb1d3578e386cbc1bd12d7b751f
SergeyRozovenko <s.rozovenko@gmail.com>
2023-05-03 23:15:50 +0300

    home work

--- scripts/script.js
+++ scripts/script.js
@@ -1,68 +1,80 @@
 const history = {
   records: [],
   get templateRecords() {
-    const template = this.records.map(
-      (record) =>
-        '<li class="record">' + JSON.stringify(record, null, 50) + "</li>"
-    );
-    return (
-      '<ul class="records card card__shadow">' + template.join(" ") + "</ul>"
-    );
+      const template = this.records.map(
+          (record) =>
+              '<li class="record">' + JSON.stringify(record, null, 50) + "</li>"
+      );
+      return (
+          '<ul class="records card card__shadow">' + template.join(" ") + "</ul>"
+      );
   },
   drawRecords() {
-    document.write(this.templateRecords);
+      document.write(this.templateRecords);
   },
+  search: (dependencies) => history.records.find(record => (
+          record.dependencies.left === dependencies.left &&
+          record.dependencies.right === dependencies.right &&
+          record.dependencies.top === dependencies.top &&
+          record.dependencies.bottom === dependencies.bottom
+  ))
 };
 
 const shape = {
   dependencies: Object.seal({
-    left: 100,
-    right: 100,
-    top: 100,
-    bottom: 100,
+      left: 100,
+      right: 100,
+      top: 100,
+      bottom: 100,
   }),
   get perimeter() {
-    //------ Bug ---
+      //------ Bug ---
+      let historyRecord = history.search(shape.dependencies);
+      if (historyRecord) {
+          console.log('Get data from history');
+          return historyRecord.perimeter;
+      }
+      console.log('New data');
 
-    //  ------ Your resolve problem there -----
+      //  ------ Your resolve problem there -----
 
-    //  ------ Your resolve problem there -----
+      //  ------ Your resolve problem there -----
 
-    // there are maybe heavy calculations
-    const total = Object.values(this.dependencies).reduce(
-      (accm, value) => accm + value,
-      0
-    );
+      // there are maybe heavy calculations
+      const total = Object.values(this.dependencies).reduce(
+          (accm, value) => accm + value,
+          0
+      );
 
-    // side effect
-    history.records.push({
-      dependencies: this.dependencies,
-      perimeter: total,
-    });
+      // side effect
+      history.records.push({
+          dependencies: this.dependencies,
+          perimeter: total,
+      });
 
-    return total;
+      return total;
   },
 
   set perimeter(perimeter) {
-    if (!Number.isInteger(perimeter) || perimeter < 400) {
-      return;
-    }
+      if (!Number.isInteger(perimeter) || perimeter < 400) {
+          return;
+      }
 
-    const size = perimeter / 4;
+      const size = perimeter / 4;
 
-    this.dependencies = Object.seal({
-      left: size,
-      right: size,
-      top: size,
-      bottom: size,
-    });
+      this.dependencies = Object.seal({
+          left: size,
+          right: size,
+          top: size,
+          bottom: size,
+      });
 
-    // side effect
+      // side effect
 
-    history.records.push({
-      dependencies: this.dependencies,
-      perimeter: perimeter,
-    });
+      history.records.push({
+          dependencies: this.dependencies,
+          perimeter: perimeter,
+      });
   },
 };
 
