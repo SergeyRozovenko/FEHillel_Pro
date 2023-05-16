85d10019d425e3b8f704465396572645ab02ac5a
SergeyRozovenko <s.rozovenko@gmail.com>
2023-05-16 23:15:22 +0300

    merge_from_release

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,16 @@ function* getPartsOfWord(string) {
     let partialWord = '';
     for (let i = 0; i < string.length; i++) {
       partialWord += string[i];
-      if (string[i] === '!' || string[i] === '@') {
-        const symbol = yield partialWord;
-        partialWord += symbol;
-      }
+      yield partialWord;
     }
-    yield partialWord;
+    
   }
   
-  const iterator = getPartsOfWord('hello');
-
-  console.log(iterator.next());
+  let iterator = getPartsOfWord("hello");
+  
+  console.log(iterator.next());  // {value: 'h', done: false}
+  console.log(iterator.next());  // {value: 'he', done: false}
+  console.log(iterator.next());  // {value: 'hel', done: false}
+  console.log(iterator.next());  // {value: 'hell', done: false}
+  console.log(iterator.next());  // {value: 'hello', done: true}
+  console.log(iterator.next());  // {value: 'hello', done: true}
