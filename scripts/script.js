8133d37c7a9650089ae9a63809786f937d9d37e7
SergeyRozovenko <s.rozovenko@gmail.com>
2023-05-17 14:50:42 +0300

    merge_from_release

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,30 @@
-function* getPartsOfWord(string) {
-    let partialWord = '';
-    let nextSymbol = '';
+
+ 
+function* getPartsOfWord (string) {
+  let partialWord = "" ;
+  let nextSymbol = "" ;
   
-    for (let i = 0; i < string.length; i++) {
-      partialWord += string[i];
-      yield { value: partialWord, done: false };
-      if (nextSymbol) {
-        partialWord += nextSymbol;
-        nextSymbol = '';
-        yield { value: partialWord, done: false };
-      }
-    }
-  
-    yield { value: partialWord, done: true };
-  }
-  
-  const iterator = getPartsOfWord("hello");
-  
-  console.log(iterator.next());  // {value: 'h', done: false}
-  console.log(iterator.next());  // {value: 'he', done: false}
-  console.log(iterator.next({ symbol: '!' }));  // {value: 'hel!', done: false}
-  console.log(iterator.next());  // {value: 'hell', done: false}
-  console.log(iterator.next({ symbol: '@' }));  // {value: 'hello@', done: false}
-  console.log(iterator.next());  // {value: 'hello', done: true}
+  for (let i = 0; i < string.length; i++) {
+    partialWord += string[i];
+    nextSymbol = yield partialWord + nextSymbol;
+
+    if (typeof nextSymbol === "undefined") {
+      nextSymbol = "";
+    };
+  };
+
+  return partialWord;
+};
+
+const iterator = getPartsOfWord('hello');
+
+console.log(iterator.next()); 
+console.log(iterator.next()); 
+console.log(iterator.next('!')); 
+console.log(iterator.next()); 
+console.log(iterator.next('@')); 
+console.log(iterator.next()); 
+
+
+
+
