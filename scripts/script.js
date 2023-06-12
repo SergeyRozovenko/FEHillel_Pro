f5334ba50e1517defb26e553ae31a5664ee89ad2
SergeyRozovenko <s.rozovenko@gmail.com>
2023-06-12 22:11:41 +0300

    add new file

--- scripts/script.js
+++ scripts/script.js
@@ -1,72 +1,64 @@
+
 function SuperMath() {
-  this.check = function(obj) {
-    if (confirm(`Ви хочете зробити дію ${obj.znak} з числами ${obj.X} і ${obj.Y}?`)) {
-      if (this.isValidOperator(obj.znak)) {
-        this.calculate(obj);
+
+  this.check = function(obj, verification) {
+      verification = (verification === undefined)
+          ? confirm(`Ви хочете зробити дію ${obj.znak} з числами ${obj.X} і ${obj.Y}`)
+          : verification;
+      
+      if(verification) {
+          this.operation(obj.X, obj.Y, obj.znak);
       } else {
-        console.log('Введений некоректний оператор!');
-      }
-    } else {
-      obj = this.input();
-      this.check(obj);
-    }
+          this.check(this.input(obj), true);
+      };
   };
 
-  this.isValidOperator = function(operator) {
-    const validOperators = ['+', '-', '/', '*', '%'];
-    return validOperators.includes(operator);
+  this.input = function (obj) {
+      obj.X = +prompt('Введіть число X:');
+      obj.Y = +prompt('Введіть число Y:');
+      obj.znak = prompt('Введіть математичну дію (+, -, /, * або %)');
+      return obj;
   };
+};
 
-  this.input = function() {
-    const newX = this.getNumberInput('Введіть число X:');
-    const newY = this.getNumberInput('Введіть число Y:');
-    const newZnak = this.getOperatorInput('Введіть математичний оператор (+, -, /, *, %):');
-    return { X: newX, Y: newY, znak: newZnak };
-  };
-
-  this.getNumberInput = function(message) {
-    let input;
-    do {
-      input = Number(prompt(message));
-    } while (isNaN(input));
-    return input;
-  };
-
-  this.getOperatorInput = function(message) {
-    let input;
-    do {
-      input = prompt(message);
-    } while (!this.isValidOperator(input));
-    return input;
-  };
-
-  this.calculate = function(obj) {
-    let result;
-    switch (obj.znak) {
+SuperMath.prototype.operation = function(a, b, znak) {
+  switch(znak) {
       case '+':
-        result = obj.X + obj.Y;
-        break;
+          alert(a + b);
+          break;
       case '-':
-        result = obj.X - obj.Y;
-        break;
+          alert(a + b);
+          break;
       case '/':
-        result = obj.X / obj.Y;
-        break;
+          if (b == 0) {
+              alert('На 0 не ділится');
+              b =  +prompt('Введіть нове значення Y:');
+              alert(a / b);
+          } else {
+              alert(a / b);
+          };
+          break;
       case '*':
-        result = obj.X * obj.Y;
-        break;
+          alert(a * b);
+          break;
       case '%':
-        result = obj.X % obj.Y;
-        break;
-      default:
-        console.log('Введений некоректний оператор!');
-        return;
-    }
-    console.log(`Результат: ${result}`);
-  };
-}
+          if (b == 0) {
+              alert('Не ділится на 0, залишок не знайдено.');
+              b =  +prompt('Введіть нове значення Y:');
+              alert(a % b);
+          } else {a
+              alert(a % b);
+          };
+          break;
+      };
+};
 
-// Приклад використання
-const obj = { X: 12, Y: 3, znak: "/" };
+const obj = {
+  X: 12,
+  Y: 3,
+  znak: '+',
+};
+
 const p = new SuperMath();
+
 p.check(obj);
