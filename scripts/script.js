f51d2ddf0b2b0743dbfb01fb7f54de3203798313
SergeyRozovenko <s.rozovenko@gmail.com>
2023-06-28 15:54:39 +0300

    add new file

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+import { strategies } from './table.js';
+
+window.onload = function() {
+    const body = document.querySelector('body');
+    
+    body.addEventListener("click", findTableStrategy)
+    
+    function findTableStrategy(event) {
+        const table = event.target.closest("tbody[data-variant-strategy]");
+
+        if (table && strategies[table.dataset.variantStrategy]) {
+            strategies[table.dataset.variantStrategy](event.target);
+        };
+    };
+};
