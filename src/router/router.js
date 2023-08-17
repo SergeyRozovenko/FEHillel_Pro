ae0d437c57857b887b0d2ae97d928503ae0df541
SergeyRozovenko <s.rozovenko@gmail.com>
2023-08-17 19:41:10 +0300

    add new file

---
+++ src/router/router.js
@@ -0,0 +1,28 @@
+import Router from "@/plugins/router";
+import Movies from "@/pages/movies/Movies";
+import SignIn from "@/pages/signIn/SignIn";
+import NotFound from "@/pages/not-found/NotFound";
+import SignUp from "@/pages/signup/SignUp";
+
+const routes = [
+    {
+        path: '/',
+        component: Movies
+    },
+    {
+        path: '/sign-in',
+        component: SignIn
+    },
+    {
+        path: '/sign-up',
+        component: SignUp
+    },
+    {
+        path: '*',
+        component: NotFound
+    },
+]
+
+
+
+export const router = new Router(routes)
